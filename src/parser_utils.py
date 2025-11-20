29bfb81ba737f596fb1e1bf5dd53c0316f2ff0ce
fleblanc <fleblanc@student.42angouleme.fr>
2025-11-20 10:02:33 +0100

    Ultimate polish 2

--- src/parser_utils.py
+++ src/parser_utils.py
@@ -3,12 +3,12 @@ import argparse
 def ratio_type(value):
     ratio = float(value)
     if ratio < 0 or ratio > 1:
-        raise argparse.ArgumentTypeError(f"Ratio must be between 0 and 1, got {ratio}")
+        raise argparse.ArgumentTypeError(f"Must be between 0 and 1, got {ratio}")
     return ratio
 
 
 def positive_int(value):
     number = int(value)
     if number < 0:
-        raise argparse.ArgumentTypeError(f"Random seed must be a positive integer, got {number}")
+        raise argparse.ArgumentTypeError(f"Must be a positive integer, got {number}")
     return number
